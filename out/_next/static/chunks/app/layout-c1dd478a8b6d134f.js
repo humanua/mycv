543b7cbc09fd20c716d22d6224adb255cb364d43
Oleksandr <mizzzin@yahoo.com>
2023-08-30 23:38:26 +0300

    Deploy

--- out/_next/static/chunks/app/layout-c1dd478a8b6d134f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{6166:function(n,e,_){Promise.resolve().then(_.t.bind(_,4531,23))},4531:function(n){n.exports={style:{fontFamily:"'__Inter_7910fd', '__Inter_Fallback_7910fd'",fontStyle:"normal"},className:"__className_7910fd"}}},function(n){n.O(0,[971,596,744],function(){return n(n.s=6166)}),_N_E=n.O()}]);
